7dba042372a374883582f829dca97724464e6466
이승재/차장/네오플러스 <voyagesj@neoplus.co.kr>
2025-11-12 16:52:21 +0900

    2.5 use client

---
+++ app/about-us/page.tsx
@@ -0,0 +1,8 @@
+import Avatar from "../../components/avatar";
+import Navigation from "../../components/navigation";
+export default function AboutUs() {
+    return (<div>
+        <Navigation />
+        about us! <Avatar />
+    </div>);
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,13 @@
-export default function Tomato(){
-    return <h1>Hello NextJS</h1>;
+import Navigation from "../components/navigation";
+
+export default function Tomato() {
+    function setCount(arg0: (c: any) => any): void {
+        throw new Error("Function not implemented.");
+    }
+
+    return (<div>
+        <Navigation />
+        <h1>Hello! NextJS</h1>
+    </div>
+    );
 }
