7ee80e5e3a6539341fa18b63fe11b015e2328a95
이승재/차장/네오플러스 <voyagesj@neoplus.co.kr>
2025-11-17 14:39:44 +0900

    error fix: Type error: Type 'MoviePageProps' does not satisfy the constraint 'PageProps'

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,26 +1,32 @@
+
 import { Suspense } from "react";
 import MovieInfo, { getMovie } from "../../../../components/movie-info";
 import MovieVideos from "../../../../components/movie-videos";
 
-interface IParams {
-    params: { id: string }
-}
+interface IParams { params: Promise<{ id: string }> }
 
-export async function generateMetadata({ params: { id } }: IParams) {
+export async function generateMetadata(props: IParams) {
+    const params = await props.params;
+    const id = params.id;
     const movie = await getMovie(id);
     return {
-        title: movie.title
+        title: movie.title,
     };
 }
 
-export default async function MovieDetail({ params: { id } }: IParams) {
-    return (<div>
-        <Suspense fallback={<h2>Loading movie info...</h2>}>
-            <MovieInfo id={id} />
-        </Suspense>
-        <Suspense fallback={<h2>Loading movie videos...</h2>}>
-            <MovieVideos id={id} />
-        </Suspense>
-    </div>
+export default async function MovieDetailPage(props: IParams) {
+    const params = await props.params;
+    const id = params.id;
+    return (
+        <div>
+            <Suspense fallback={<h1>Loading movie info</h1>}>
+                <MovieInfo id={id} />
+            </Suspense>
+            <Suspense fallback={<h1>Loading movie videos</h1>}>
+                <MovieVideos id={id} />
+            </Suspense>
+        </div>
     );
 }
+
+export const runtime = "edge";
--- components/navigation.tsx
+++ components/navigation.tsx
@@ -1,8 +1,7 @@
-"use client"
+"use client";
+
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { useState } from "react";
-
 import styles from "../styles/navigation.module.css";
 
 export default function Navigation() {
@@ -11,10 +10,11 @@ export default function Navigation() {
         <nav className={styles.nav}>
             <ul>
                 <li>
-                    <Link href="/">Home</Link> {path === "/" ? "🙃" : ""}
+                    <Link href="/">Home</Link> {path === "/" ? "🔥" : ""}
                 </li>
                 <li>
-                    <Link href="/about-us">About us</Link> {path === "/about-us" ? "🙃" : ""}
+                    <Link href="/about-us">About Us</Link>
+                    {path === "/about-us" ? "🔥" : ""}
                 </li>
             </ul>
         </nav>
